f5bb87dd2e95a07fe02496048f3e630e5da2d831
Anvita Dixit <anvita.dixit@tutree.com>
2020-03-19 01:57:26 +0530

    font size add

--- src/styles/fontSize.js
+++ src/styles/fontSize.js
@@ -1,7 +1,10 @@
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 const fontSize = {
   screenTitle: 22,
   subcategory: {title: 11},
   menu: {title: 15},
   priceBreakup: {price: 9},
+  dashboard: {title: hp('1.2%')},
 };
 export default fontSize;
